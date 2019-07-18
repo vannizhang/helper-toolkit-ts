446264e9d7cb8dc0ba8884f2467a47e9c2317a99
Jinnan Zhang <vannizhang@gmail.com>
2019-07-18 12:17:09 -0700

    1.1.2

--- src/url/index.ts
+++ src/url/index.ts
@@ -53,37 +53,38 @@ const parsePairsFromString = (inputStr = '') => {
 };
 
 interface IUpdateKeyValuePairInQueryStringParams {
-  queryString:string,
-  key:string,
-  value:string | boolean | number
+  queryString: string;
+  key: string;
+  value: string | boolean | number;
 }
 
-const updateKeyValuePairInQueryString = ({ queryString, key, value }:IUpdateKeyValuePairInQueryStringParams): string =>{
-
-  if(queryString && key){
+const updateKeyValuePairInQueryString = ({
+  queryString,
+  key,
+  value,
+}: IUpdateKeyValuePairInQueryStringParams): string => {
+  if (queryString && key) {
     const queryParams = parsePairsFromString(queryString);
 
     queryParams[key] = value.toString();
 
     const queryParamsStr = stringifyQueryParams(queryParams);
 
-    return queryParamsStr
+    return queryParamsStr;
   }
 
   return '';
-}
+};
 
-const stringifyQueryParams = (queryParams: any): string =>{
-
+const stringifyQueryParams = (queryParams: any): string => {
   const queryParamsStr = Object.keys(queryParams)
     .map((paramKey: any) => {
-      const val:any = queryParams[paramKey];
+      const val: any = queryParams[paramKey];
       return `${paramKey}=${val}`;
     })
     .join('&');
 
   return queryParamsStr;
-}
-
+};
 
 export { parseQuery, parseHash, updateQueryParamInUrl, updateQueryParam, updateKeyValuePairInQueryString };
