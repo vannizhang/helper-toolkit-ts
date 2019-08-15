13bbbb56d6c9a3b0a26f6e5569a6b0e2b843f407
Jinnan Zhang <vannizhang@gmail.com>
2019-08-15 14:01:21 -0700

    add getMonthName func

--- src/date/index.ts
+++ src/date/index.ts
@@ -2,4 +2,15 @@ const addMinutesToCurrentTime = (numOfMinToAdd: number) => {
   return new Date().getTime() + numOfMinToAdd * 60 * 1000;
 };
 
-export { addMinutesToCurrentTime };
+const getMonthName = (monthIndex=-1, isShort=false)=>{
+  const names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const shortNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  if(monthIndex===-1){
+    return '';
+  }
+
+  return isShort ? shortNames[monthIndex] : names[monthIndex];
+};
+
+export { addMinutesToCurrentTime, getMonthName };
