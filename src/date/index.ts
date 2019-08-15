c8e21083de8e5792221e669631a60f81010cd3d1
Jinnan Zhang <vannizhang@gmail.com>
2019-08-15 14:02:08 -0700

    1.1.4

--- src/date/index.ts
+++ src/date/index.ts
@@ -2,11 +2,24 @@ const addMinutesToCurrentTime = (numOfMinToAdd: number) => {
   return new Date().getTime() + numOfMinToAdd * 60 * 1000;
 };
 
-const getMonthName = (monthIndex=-1, isShort=false)=>{
-  const names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const getMonthName = (monthIndex = -1, isShort = false) => {
+  const names = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
   const shortNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-  if(monthIndex===-1){
+  if (monthIndex === -1) {
     return '';
   }
 
